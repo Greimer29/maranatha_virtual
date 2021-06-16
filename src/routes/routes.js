35926e11f1f4fce12c7c4cdbed34c9ccfd0b2c4a
GreimerKmon <greimerabelguzman29@gmail.com>
2021-06-16 09:36:27 -0700

    Respondiendo al acceder a una ruta

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
-
+router.get('/',(req,res)=>{
+    res.json({
+        status:'API works normaly'
+    })
+})
 
 module.exports = router;
