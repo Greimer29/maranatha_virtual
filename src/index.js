ffe5da6d3bb9cad7faad8ac99ec31c90748db3be
GreimerKmon <greimerabelguzman29@gmail.com>
2021-06-16 14:45:32 -0700

    Sirviendo mis archivos estáticos

--- src/index.js
+++ src/index.js
@@ -1,16 +1,20 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const path = require('path');
 
 //middlewares
 app.use(morgan('dev'));
 app.use(express.json());
 
 //routes
-app.use(require('./routes/routes'));
+app.use('api/',require('./routes/routes'));
 
 //settings 
 app.set('port', process.env.PORT || 3000 );
+
+//statics files
+app.use(express.static(path.join(__dirname, 'public')))
 
 //starting the server
 app.listen(app.get('port'),()=>{
