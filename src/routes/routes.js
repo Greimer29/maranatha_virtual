e7def53198f21b218a9f77f4ec5683f826923e25
GreimerKmon <greimerabelguzman29@gmail.com>
2021-06-16 17:27:59 -0700

    Definiendo la petición GET de mis años

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const router = express.Router();
+const Anno = require('../models/models');
 
-router.get('/',(req,res)=>{
-    res.json({
-        status:'API works normaly'
-    })
+router.get('/api/anno', async (req,res)=>{
+    try {
+        const anno = await Anno.find();
+        // console.log(ano);
+        res.json(anno)
+    }catch(err){
+        err = new Error('Algo ha salido mal');
+        console.error(err);
+    }
 })
 
 module.exports = router;
