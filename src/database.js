e219a03d9f7f4b94750010c820ff0829b547b87a
GreimerKmon <greimerabelguzman29@gmail.com>
2021-06-16 17:29:29 -0700

    Message por la consola

--- src/database.js
+++ src/database.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const URI = 'mongodb://localhost/maranatha';
 
 mongoose.connect(URI)
-.then(db=> console.log(`Oh yeah boy`))
+.then(db=> console.log(`DB is conected`))
 .catch(err => console.error(err))
 
 module.exports=mongoose;
