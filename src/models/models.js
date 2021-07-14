10f8df901544f054a5eb0bd7a9a792fcce2101a9
GreimerKmon <greimerabelguzman29@gmail.com>
2021-07-14 14:15:08 -0700

    Un intento deseperado de que agarre como deseo

--- src/models/models.js
+++ src/models/models.js
@@ -1,8 +1,17 @@
 const mongoose = require('mongoose');
 const {Schema} = mongoose;
 
-const annoSchema = new Schema ({
-    name:{type:String,required:true}
-});
+const materiaSchema = new Schema ({
+    name:{type:String, required:true},
+    anno: [{type: Schema.Types.ObjectId, ref:'anno'}]
+})
 
-module.exports = mongoose.model('anno',annoSchema);
+const Materia = mongoose.model('Materia', materiaSchema);
+
+const annoSchema = new Schema ({
+    name:{type:String,required:true},
+    materias:[{type: Schema.Types.ObjectId, ref:'Materia'}]
+});
+const Anno = mongoose.model('anno',annoSchema);
+
+module.exports = {Anno, Materia}
